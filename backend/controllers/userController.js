21a13644f73fcd0fd9d9d7836fced2cf5cd8830f
Massika-Ke <wamwoka99@gmail.com>
2025-01-23 01:20:27 +0300

    Initial Commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,9 +9,48 @@ const loginUser = async (req, res) =>{
 
 }
 
+const createToken = (id)=>{
+    return jwt.sign({id}, process.env.JWT_SECRET)
+}
+
 // register user
 const registerUser = async (req, res)=>{
+    const {name, password, email} = req.body;
+    try {
+        // check if user already exists
+        const exists = await userModel.findOne({email});
+        if (exists) {
+            return res.json({success:false, message: "User Already Exists"})
+        }
 
+        // validate email format and password
+        if (!validator.isEmail(email)) {
+            return res.json({success:false, message: "Please Enter a valid Email"})
+        }
+
+        if (password.length>8) {
+            return res.json({success:false, message:"Password must be 8 characters"})
+        }
+
+        // Hashing user password
+        const salt = await  bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // create new user
+        const newUser = new userModel({
+            name:name,
+            email:email,
+            password:password
+        })
+        // save the user
+        const user = await newUser.save()
+        const token = createToken(user_id)
+        res.json({success:true, token});
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: "Error"})
+    }
 }
 
 export {loginUser, registerUser};
