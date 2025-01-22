b1ca32f36793bf040107ff340a8d079b1717ccc5
Massika-Ke <wamwoka99@gmail.com>
2025-01-23 00:33:46 +0300

    Initial Commit

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -2,26 +2,7 @@
 const Orders = () => {
   return (
     <div>
-      <div className="list-table">
-        <div className="list-table-format title">
-          <b>Image</b>
-          <b>Name</b>
-          <b>Category</b>
-          <b>Price</b>
-          <b>Action</b>
-        </div>
-        {list.map((item,index)=>{
-          return (
-            <div key={index} className="list-table-format">
-              <img src={`${url}/images/`+item.image} alt="" />
-              <p>{item.name}</p>
-              <p>{item.category}</p>
-              <p>{item.price}</p>
-              <p onClick={()=>removeFood(item._id)} className='cursor'>X</p>
-            </div>
-          )
-        })}
-      </div>
+      
     </div>
   )
 }
