a238b06132c8b9efa139a6f4db49095d9244b932
Massika-Ke <wamwoka99@gmail.com>
2025-01-25 23:05:19 +0300

    Initial Commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,30 @@ import validator from "validator"
 
 // login User
 const loginUser = async (req, res) =>{
+    const {email, password} = req.body;
 
+    try {
+        const user = await userModel.findOne({email});
+
+        if (!user) {
+            return res.json({success:false, message:"User does'nt exist"});
+        }
+// validate password 
+        const isMatch = await bcrypt.compare(password, user.password);
+        
+        if (!isMatch) {
+            return res.json({success:false, message: "Invalid Credentials"})
+        }
+
+        const token = createToken(user._id);
+        res.json({success:true, token})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: "Error"})
+        
+    }
+    
 }
 
 const createToken = (id)=>{
