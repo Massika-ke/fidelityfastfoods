e9998f80b5f6f0679787cb703c74854517c3dbe3
Massika-Ke <wamwoka99@gmail.com>
2025-02-16 23:46:47 +0300

    Initial Commit

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,11 +1,26 @@
 import orderModel from "../models/orderModel";
 import userModel from "../models/userModel";
-import stripe from "stripe"
+import Stripe from "stripe"
 
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // Placing an order from frontend
 const placeOrder = async (req, res)=>{
 
+    try {
+        const newOrder = new orderModel({
+            userId: req.body.userId,
+            items:req.body.items,
+            amount: req.body.amount,
+            address: req.body.address
+        })
+        await newOrder.save();
+        await userModel.findByIdAndUpdate(req.body.userId, {cartData:{}})
+
+    } catch (error) {
+        
+    }
 }
 
 export {placeOrder}
