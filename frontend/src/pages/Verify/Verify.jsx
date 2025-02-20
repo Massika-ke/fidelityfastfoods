bf2411c49abe6a1714482fa6ef70e9e1ab246518
Massika-Ke <wamwoka99@gmail.com>
2025-02-20 23:48:33 +0300

    verify-loader

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,11 +1,21 @@
+import { useSearchParams } from "react-router-dom";
 import "./Verify.css"
+import { useContext } from "react";
+import { StoreContext } from "../../context/StoreContext";
 
 
 const Verify = () => {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const success = searchParams.get("success")
+    const orderId = searchParams.get("orderId")
+    const {url} = useContext(StoreContext);
+    
   return (
-    <div>
-        nothing...
-            console.log(kenya);
+    <div className="verify">
+       <div className="spinner">
+
+       </div>
     </div>
     
   )
