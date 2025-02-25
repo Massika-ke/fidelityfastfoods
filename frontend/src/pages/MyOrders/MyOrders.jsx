52ad5f7ad6ca3eddbbfcc36b72050da15616d6df
Massika-Ke <wamwoka99@gmail.com>
2025-02-25 23:50:22 +0300

    Initial Commit

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { StoreContext } from '../../context/StoreContext'
+import axios from 'axios';
 
 const MyOrders = () => {
+
+    const {url, token} = useContext(StoreContext);
+    const [data, setData] = useState([]);
+
+    const fetchOrders = async ()=>{
+        const response = await axios.post(
+            url+ "/api/order/userorders", {}, {headers:{token}}
+        );
+        setData(response.data.data);
+        
+    }
+
+    useEffect(()=>{
+        if (token) {
+            fetchOrders();
+        }
+    }, [token])
+
   return (
-    <div>MyOrders</div>
+    <div className='my-orders'>
+        <h2>My Orders</h2>
+        <div className="container">
+            {data.map((order, index)=>{
+                return (
+                    <div key={index} className="my-orders-order">
+                     <img src="" alt="" />   
+                    </div>
+                )
+            })}
+        </div>
+    </div>
   )
 }
 
