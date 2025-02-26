c88ecd4cf376cb67a19347d675229fefdb0be5ec
Massika-Ke <wamwoka99@gmail.com>
2025-02-26 21:54:46 +0300

    Initial Commit

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react'
 import { StoreContext } from '../../context/StoreContext'
 import axios from 'axios';
+import { assets } from '../../assets/assets';
 
 const MyOrders = () => {
 
@@ -28,7 +29,19 @@ const MyOrders = () => {
             {data.map((order, index)=>{
                 return (
                     <div key={index} className="my-orders-order">
-                     <img src="" alt="" />   
+                     <img src={assets.parcel_icon} alt="" />  
+                     <p>{order.items.map((item, index)=>{
+                        if (index === order.items.length - 1) {
+                            return item.name + " x " +item.quantity
+                        }
+                        else{
+                            return item.name+ " x " + item.quantity + ","
+                        }
+                     })}</p> 
+                     <p>${order.amount}.00</p>
+                     <p>Items: {order.items.length}</p>
+                     <p><span>&#x25cf;</span> <b>{order.status}</b> </p>
+                     <button>Track Order</button>
                     </div>
                 )
             })}
